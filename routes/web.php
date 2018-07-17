83fb95037c915ddbafdae7803b9f25ccd5f57b05
root <root@ip-172-31-47-175.ap-northeast-1.compute.internal>
2018-07-17 16:55:49 +0000

    migration

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,23 @@
 |
 */
 
+/**
+ * Show Task Dashboard
+ **/
 Route::get('/', function () {
-    return view('welcome');
+  return view('tasks');
+});
+
+/**
+ * Add new Task
+ **/
+Route::post('/task', function (Request $request) {
+  //
+});
+
+/**
+ * Delete Task
+ **/
+Route::get('/task/{task}', function (Task $task) {
+  //
 });
