3c40fd5c075bea2ec53cf0147879cd93aa8d393c
root <root@ip-172-31-47-175.ap-northeast-1.compute.internal>
2018-07-21 13:47:37 +0000

    add insert, delete

--- routes/web.php
+++ routes/web.php
@@ -23,8 +23,26 @@ use Illuminate\Http\Request;
     ]);
   });
 
-  Route::post('task', function (Request $request) {
-    return view('tasks', [
-        'tasks' => Task::orderBy('created_at', 'asc')->get()
+  Route::post('/task', function (Request $request) {
+    $validator = Validator::make($request->all(), [
+        'name' => 'required|max:255',
     ]);
+
+    if ($validator->fails()) {
+        return redirect('/')
+            ->withInput()
+            ->withErrors($validator);
+    }
+
+    $task = new Task;
+    $task->name = $request->name;
+    $task->save();
+
+    return redirect('/');
+  });
+
+  Route::delete('/task/{id}', function ($id) {
+    Task::findOrFail($id)->delete();
+
+    return redirect('/');
   });
